a6bf82382f1fd7febf9dec2f4120f11191609cf8
YogiAfrilisca95 <yogi.lenovoa1000@gmail.com>
2025-04-27 19:35:46 +0700

    fix projectdetail

--- script.js
+++ script.js
@@ -1,51 +1,9 @@
-// Typing Effect
-const typedTextSpan = document.querySelector(".typing");
-const words = ["I am not a Programmer:)", "I am not a UI/UX Designer:)", "I am not a Developer:)"];
-let wordIndex = 0;
-let charIndex = 0;
-let typingDelay = 100;
-let erasingDelay = 60;
-let newWordDelay = 1500; // Delay between words
+const modeToggle = document.getElementById('modeToggle');
+const htmlElement = document.documentElement;
 
-function type() {
-    if (charIndex < words[wordIndex].length) {
-        typedTextSpan.textContent += words[wordIndex].charAt(charIndex);
-        charIndex++;
-        setTimeout(type, typingDelay);
-    } else {
-        setTimeout(erase, newWordDelay);
-    }
-}
-
-function erase() {
-    if (charIndex > 0) {
-        typedTextSpan.textContent = words[wordIndex].substring(0, charIndex - 1);
-        charIndex--;
-        setTimeout(erase, erasingDelay);
-    } else {
-        wordIndex++;
-        if (wordIndex >= words.length) wordIndex = 0;
-        setTimeout(type, typingDelay + 500);
-    }
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-    if (words.length) setTimeout(type, newWordDelay);
-});
-
-// Handle Contact Form
-document.getElementById('contact-form').addEventListener('submit', function (event) {
-    event.preventDefault();
-
-    const name = document.getElementById('name').value.trim();
-    const email = document.getElementById('email').value.trim();
-    const message = document.getElementById('message').value.trim();
-
-    if (name === "" || email === "" || message === "") {
-        alert("Semua field wajib diisi!");
-        return;
-    }
-
-    alert(`Terima kasih, ${name}! Pesan Anda telah dikirim.`);
-    this.reset();
+modeToggle.addEventListener('click', () => {
+    const currentTheme = htmlElement.getAttribute('data-bs-theme');
+    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+    htmlElement.setAttribute('data-bs-theme', newTheme);
+    modeToggle.textContent = newTheme === 'light' ? '🌙' : '☀️';
 });
